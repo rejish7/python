51f410c188247fd56de29cd6e5d224a42ae08018
Rejish <khanalrejish232gmail.com>
2024-06-12 22:15:24 +0545

    python function

---
+++ main/function/nested_loop.py
@@ -0,0 +1,25 @@
+#wap to show the loop
+#*
+#**
+#***
+#****
+
+
+for i  in range(7,-1,-1):
+    if i ==4:
+        print('*' + 'moon' + '*')
+    else: 
+        print('*' * (i+1)) 
+    print()
+
+for i  in range(1,7):
+    if i == 4:
+        print('*' + 'SUN' + '*')
+    else:
+        print('*' * (i+1))
+        
+
+
+
+
+    
