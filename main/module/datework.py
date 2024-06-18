4781156e5ae2f34e697dd6f1f4a7d43786205573
Rejish khanal <khanalrejish23@gmail.com>
2024-06-18 07:05:19 +0545

    third commit

--- main/module/datework.py
+++ main/module/datework.py
@@ -1,10 +1,11 @@
 import datetime
-jobs =[ 
-    {'title':'Quality Assurance','company':'microsoft','exp_date':'2024-02-23'}
-    {'title':'software Developer','company':'google','exp_date':'2025-02-23'} 
-    {'title':'Data scientist','company':'TCS','exp_date':'2026-02-23'}
-    {'title':'data Analyst','company':'Facebook','exp_date':'2023-02-23'}
+jobs =[
+{'title':'Quality Assurance','company':'microsoft','exp_date':'2024-02-23'},
+{'title':'Software Developer','company':'google','exp_date':'2025-02-23'},
+{'title':'Data scientist','company':'TCS','exp_date':'2026-02-23'},
+{'title':'Data Analyst','company':'Facebook','exp_date':'2023-02-23'}
 ]
+    
 
 
 for job in jobs:
@@ -12,7 +13,7 @@ for job in jobs:
     today  = datetime.datetime.now()
     if exp_date< today:
         exp_date = today- exp_date
-        print(f"{job['titile']} jobs in {job['company']} is expired {exp_date.days}days ago")
+        print(f"{job['title']} jobs in {job['company']} is expired {exp_date.days} days ago")
     else:
         active_days = exp_date-today
-        print(f"{job['titile']} jobs in {job['company']}{active_days} is active")
+        print(f"{job['title']} jobs in {job['company']}{active_days} is active")
